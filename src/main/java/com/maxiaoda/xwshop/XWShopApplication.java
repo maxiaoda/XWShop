c882d6863f7eb7ee3409c385818e9923ddedebdb
maxiaoda <631677979@qq.com>
2020-05-03 11:02:55 +0800

    Update:创建项目框架 使用了spring、mybatis、flyway自动化创建了项目的基本框架

---
+++ src/main/java/com/maxiaoda/xwshop/XWShopApplication.java
@@ -0,0 +1,13 @@
+package com.maxiaoda.xwshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XWShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XWShopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/maxiaoda/xwshop/XWShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maxiaoda.xwshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XWShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
